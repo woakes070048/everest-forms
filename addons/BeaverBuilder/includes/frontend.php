e8327b64153264b2d74141c44587fc9268fafeae
riteelama <riteelama@gmail.com>
2024-11-27 14:15:10 +0545

    Fix - Beaver builder render in the frontend

--- addons/BeaverBuilder/includes/frontend.php
+++ addons/BeaverBuilder/includes/frontend.php
@@ -18,13 +18,30 @@ if ( ! empty( $settings_attr['form_selection'] ) && ! empty( $form_list ) ) {
 } else {
 	$image_path = evf()->plugin_url() . '/assets/images/everest-forms-logo.png';
 	$class      = 'everest-forms-logo__beaver';
-	$render     = '<div class="everest-forms-beaver__container">';
-	$render    .= '<img src="' . esc_url( $image_path ) . '" class="' . esc_attr( $class ) . '" alt="Everest Forms Logo" />';
+
 
 	if ( empty( $form_list ) ) {
-		$render .= '<p>' . esc_html__( 'Seems like you haven\'t created a form. Please create one to use it.', 'everest-forms' ) . '</p>';
+		$page_id      = get_the_ID();
+		$page_content = get_post_field( 'post_content', $page_id );
+		$page_meta    = get_post_meta( $page_id, '_fl_builder_data', true );
+		foreach ( $page_meta as $node ) {
+			if ( isset( $node->settings ) && isset( $node->settings->form_selection ) ) {
+				$form_selection = $node->settings->form_selection;
+			}
+		}
+		if ( ! empty( $form_selection ) ) {
+			$render = EVF_Shortcodes::form( $form_atts );
+		} else {
+			$render  = '<div class="everest-forms-beaver__container">';
+			$render .= '<img src="' . esc_url( $image_path ) . '" class="' . esc_attr( $class ) . '" alt="Everest Forms Logo" />';
+			$render .= '<p>' . esc_html__( 'Seems like you haven\'t created a form. Please create one to use it.', 'everest-forms' ) . '</p>';
+			$render .= '</div>';
+		}
 	} else {
+		$render  = '<div class="everest-forms-beaver__container">';
+		$render .= '<img src="' . esc_url( $image_path ) . '" class="' . esc_attr( $class ) . '" alt="Everest Forms Logo" />';
 		$render .= '<p>' . esc_html__( 'Please select a form', 'everest-forms' ) . '</p>';
+		$render .= '</div>';
 	}
 
 	$render .= '</div>';
